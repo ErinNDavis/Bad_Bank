1fe43635cf130518a2ff85f11337ce80d0ed7ebc
Erin Davis <davise25@winthrop.edu>
2021-07-05 18:04:24 -0400

    structural updates

---
+++ index.js
@@ -0,0 +1,13 @@
+function Spa(){
+    return (
+        <>
+        <h1>Welcome to bad bank </h1>
+        <NavBar/>
+        </>
+    );
+}
+
+ReactDOM.render(
+    <Spa/>,
+    document.getElementById('root')
+)
